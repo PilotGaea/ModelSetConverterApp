f1c84b7c5665d13273887a38e748f1154e5be42b
louis.li <a6693432@gmail.com>
2021-07-12 12:01:36 +0800

    init

---
+++ Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿
+namespace ModelSetConverterApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.m_MessageList = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // m_MessageList
+            // 
+            this.m_MessageList.FormattingEnabled = true;
+            this.m_MessageList.ItemHeight = 15;
+            this.m_MessageList.Location = new System.Drawing.Point(12, 12);
+            this.m_MessageList.Name = "m_MessageList";
+            this.m_MessageList.Size = new System.Drawing.Size(776, 334);
+            this.m_MessageList.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 365);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(776, 73);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.m_MessageList);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox m_MessageList;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using PilotGaea.TMPEngine;
+
+namespace ModelSetConverterApp
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            CModelSetMaker maker = new CModelSetMaker();
+
+            maker.ProgressPercentChanged += Maker_ProgressPercentChanged;
+            maker.ProgressMessageChanged += Maker_ProgressMessageChanged;
+            maker.CreateLayerCompleted += Maker_CreateLayerCompleted;
+
+            string[] FieldNames = new string[2];
+            FieldNames[0] = "Field1";
+            FieldNames[1] = "Field2";
+            FIELD_TYPE[] FieldTypes = new FIELD_TYPE[2];
+            FieldTypes[0] = FIELD_TYPE.STRING;
+            FieldTypes[1] = FIELD_TYPE.DOUBLE;
+            if (maker.Create("layername", @"D:\圖資\test\ConverterDB.DB", "terrain", @"D:\圖資\terrain.DB", EXPORT_TYPE.LET_DB, FieldNames, FieldTypes))
+            {
+
+            }
+            MODELSET_SRC src = new MODELSET_SRC("KMZ");
+            src.MeshFileName = @"D:\圖資 \Taipei3DBuilding\4847\5144_r1.kmz";
+            src.MeshEPSG = 3826;
+
+            src.Attrs = new string[0];
+            src.OffsetX = 0;
+            src.OffsetY = 0;
+            src.OffsetZ = 0;
+            src.PosX = 0;
+            src.PosY = 0;
+            src.PosZ = 0;
+            src.IsAbsHeight = false;
+            maker.NewEntity(src);
+
+            maker.EndCreate(true);
+
+            maker = null;
+        }
+
+        private void Maker_CreateLayerCompleted(string LayerName, bool Success, string ErrorMessage)
+        {
+            m_MessageList.Items.Add("Maker_CreateLayerCompleted:" + LayerName + " - " + Success.ToString());
+        }
+
+        private void Maker_ProgressMessageChanged(string Message)
+        {
+            m_MessageList.Items.Add("Maker_ProgressMessageChanged:" + Message);
+        }
+
+        private void Maker_ProgressPercentChanged(double Percent)
+        {
+            m_MessageList.Items.Add("Maker_ProgressPercentChanged:" + (Percent / 10.0).ToString("0.00") + "%");
+        }
+
+    }
+}
